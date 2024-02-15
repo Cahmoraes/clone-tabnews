b44881d001296767ee4899b6fdb00dce5753fb38
Caique Moraes <ike.moraes93@gmail.com>
2024-02-15 05:53:12 -0300

    makes database.js client.connect() more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
