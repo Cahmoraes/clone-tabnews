efbe33a7364287f6f210294decb340ac67b98cad
Caique Moraes <ike.moraes93@gmail.com>
2024-02-15 07:02:09 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Credenciais do Postgres", {
+    env: process.env.NODE_ENV,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
