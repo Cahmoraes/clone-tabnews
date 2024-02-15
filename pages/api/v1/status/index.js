10c58151f4f232275beb9ca8736ea1e502e13b6b
Caique Moraes <ike.moraes93@gmail.com>
2024-02-15 05:58:43 -0300

    update config

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -18,7 +18,6 @@ export default async function status(request, response) {
 
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
-
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
