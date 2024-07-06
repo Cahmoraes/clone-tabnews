fb9b4c26b374f3cdbc07f522f442668bedc114e2
Caique Moraes <ike.moraes93@gmail.com>
2024-07-06 14:47:23 -0300

    adds linting GitHub Action

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,16 +1,16 @@
-const { exec } = require('node:child_process')
+const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)  
-  
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
   function handleReturn(error, stdout) {
-    if(stdout.search('accepting connections') === -1) {
-      process.stdout.write('.')
-      checkPostgres()
-      return
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
     }
-    console.log("\n\n🍏 Postgres está pronto e aceitando conexões!\n")
+    console.log("\n\n🍏 Postgres está pronto e aceitando conexões!\n");
   }
 }
-process.stdout.write('\n\n🚀 Aguardando Postgres aceitar conexões')
-checkPostgres()
+process.stdout.write("\n\n🚀 Aguardando Postgres aceitar conexões");
+checkPostgres();
