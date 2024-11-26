561448a9ba245673545c0481dfbe00c3d2f12e54
Caique Moraes <ike.moraes93@gmail.com>
2024-11-26 12:35:36 -0300

    feat: add initial verion of '/status' page

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,8 +11,8 @@ describe("GET /api/v1/status", () => {
       expect(response.status).toBe(200);
       expect(response.ok).toBe(true);
       const responseBody = await response.json();
-      const parsedDate = new Date(responseBody.updated_at).toISOString();
-      expect(responseBody.updated_at).toEqual(parsedDate);
+      const parsedUpdatedDate = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedDate);
       expect(responseBody.dependencies.database.version).toEqual("16.2");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.opened_connections).toBe(1);
