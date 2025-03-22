6fee8695c6a8b9afe07b54009133f9e3ebca52c7
Caique Moraes <ike.moraes93@gmail.com>
2025-03-21 21:58:39 -0300

    feat: create and use NotFoundError in controller.js

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,24 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message ?? "Não foi possível encontrar este recurso no sistema.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action ?? "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
