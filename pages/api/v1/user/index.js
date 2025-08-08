d9c99eee7f187ef26cf1523ee20c929c33c0fd40
Caique Moraes <ike.moraes93@gmail.com>
2025-08-08 10:14:50 -0300

    feat: add Cache-Control header to user retrieval response and update tests

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -14,5 +14,9 @@ async function getHandler(request, response) {
   const renewedSessionObject = await session.renew(sessionObject.id);
   controller.setSessionCookie(renewedSessionObject.token, response);
   const userFound = await user.findOneById(sessionObject.user_id);
+  response.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, max-age=0, must-revalidate",
+  );
   return response.status(200).json(userFound);
 }
